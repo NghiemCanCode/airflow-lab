8d368fc336cb0dce8f0721990a9b1d4a3da0807f
NghiemCanCode <nghiemgankteam@gmail.com>
2026-08-01 15:50:36 +0000

    feat: day1 to learning airflow.

---
+++ week_1/day_1/dags/hello_airflow.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+from datetime import datetime
+
+def say_hello():
+    print("Hello Airflow!")
+
+with DAG(
+    dag_id="hello_airflow",
+    start_date=datetime(2025,1,1),
+    schedule=None,
+    catchup=False,
+) as dag:
+
+    hello = PythonOperator(
+        task_id="say_hello",
+        python_callable=say_hello,
+    )
